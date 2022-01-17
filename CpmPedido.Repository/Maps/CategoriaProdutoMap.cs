10f1791cd3bd6797bb7d8cd53a1a02b2e1ff6496
Cleverson Fukuoka <cleversonfukuoka@hotmail.com>
2022-01-16 21:00:56 -0300

    Add files via upload

---
+++ CpmPedido.Repository/Maps/CategoriaProdutoMap.cs
@@ -0,0 +1,21 @@
+﻿using CpmPedido.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CpmPedido.Repository
+{
+    public class CategoriaProdutoMap : BaseDomainMap<CategoriaProduto>
+    {
+        public CategoriaProdutoMap() : base("tb_categoria_produto") { }
+
+        public override void Configure(EntityTypeBuilder<CategoriaProduto> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Nome).HasColumnName("nome").HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Ativo).HasColumnName("ativo").IsRequired();
+
+        }
+    }
+}
+
---
+++ CpmPedido.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using CpmPedido.Interface;
+
+namespace CpmPedido.Repository
+{
+    public class BaseRepository
+    {
+        protected const int TamanhoPagina = 5;
+
+        protected readonly ApplicationDbContext DbContext;
+
+        public BaseRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
